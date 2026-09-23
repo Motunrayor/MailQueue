a861e9575a70ea594999a2c31b04d794ecb64f9e
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-09-23 12:40:09 +0100

    docs: updated gitignore to contain all things we need to ignore and installed nodemon as a dependencies

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,16 @@
-function server() {}
+const cors = require('cors');
 
-module.exports = server;
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:5000', 'http://127.0.0.1:5000']
+}))
+
+
+app.listen(5000, () => {
+    console.log('The Server is running on port 5000');
+});
